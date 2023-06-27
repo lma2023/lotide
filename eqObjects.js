36fdde2ce495591ed89cd167f21b3b872dd8e140
lma2023 <lyassovam@gmail.com>
2023-06-27 16:54:30 -0700

    implement key Objects

---
+++ eqObjects.js
@@ -0,0 +1,27 @@
+const eqArrays = require("./eqArrays");
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  for (const item in object1) {
+    if (Array.isArray(object1[item])) {
+      if (!eqArrays(object1[item], object2[item])) {
+        return false;
+      }
+    } else if (object1[item] !== object2[item]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+console.log(eqObjects(shirtObject, anotherShirtObject));
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+console.log(eqObjects(shirtObject, longSleeveShirtObject));
+
+module.exports = eqObjects;
