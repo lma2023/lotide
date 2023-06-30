85078fc069d0f96a1290306d1e67c91b47edef34
lma2023 <lyassovam@gmail.com>
2023-06-30 16:53:15 -0700

    Implement map

---
+++ map.js
@@ -0,0 +1,18 @@
+const assertArraysEqual = require("./AssertArrayEqual");
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const num = [1, 2, 3];
+
+console.log(map(words, word => word[0]));
+let example2 = map(num, (num) => num * 10);
+assertArraysEqual(example2, [10, 20, 30, 40, 50]);
+
+module.exports = map;
