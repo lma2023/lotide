05f86379871ab6b92e8b994e438b611325912a4c
lma2023 <lyassovam@gmail.com>
2023-08-03 18:08:11 -0700

    Modified

--- letterPositions.js
+++ letterPositions.js
@@ -1,8 +1,5 @@
 
-const assertArraysEqual = require("./AssertArrayEqual");
-
-eqArrays(assertArraysEqual([1, 2, 3], [1, 2, 5]), true);
-module.exports = assertArraysEqual;
+const assertArraysEqual = require("./assertArrayEqual");
 const letterPositions = function(sentence) {
     const results = {};
     for (let i = 0; i < sentence.length; i++) {
