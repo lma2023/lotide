5363a8a9a665c402007523d424b00dc2db8c9a71
lma2023 <lyassovam@gmail.com>
2023-08-04 09:29:20 -0700

    middle element of array

--- middle.js
+++ middle.js
@@ -1,12 +1,14 @@
 const middle = function(array) {
     let arrayResult = [];
-    if (array.length <= 2 && array.length > 0) {
+    if (array.length === 0) {
+      return arrayResult;
+    } if (array.length <= 2 && array.length > 0) {
       return arrayResult;
     } else if (array.length % 2 === 0) {
       return arrayResult = [array[(array.length / 2) - 1], array[array.length / 2]];
     } else {
-      return arrayResult = array[(array.length - 1) / 2];
+      return arrayResult = [array[(array.length - 1) / 2]];
     }
   };
-  console.log(middle([1, 2, 3, 4, 5, 6]));
+  console.log(middle([]));
   module.exports = middle;
