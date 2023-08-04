4d70825520be2eca4f674f2538b7b92f49db9633
lma2023 <lyassovam@gmail.com>
2023-08-04 09:28:17 -0700

    comparing arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,7 @@
-const assertEqual = require('./AssertEqual');
+const assertEqual = require('./assertEqual');
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    return console.log('false');
+    return false;
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
