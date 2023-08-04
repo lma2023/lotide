2c195be56210b8419e107ad64121b8c51b2d998b
lma2023 <lyassovam@gmail.com>
2023-08-03 17:56:47 -0700

    Modifed

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected){
-      console.log(`Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`Assertion Failed: ${actual} !== ${expected}`);
-    }
-};
+const assertEqual = require('./assertEqual');
 const countOnly = function(allItems, itemsToCount) {
   let result = {};
   for (let item of allItems) {
@@ -32,4 +26,6 @@ const firstNames = [
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 assertEqual(result1["Karima"], undefined);
+
+module.exports = countOnly;
 
